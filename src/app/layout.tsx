474edd3bd1fcc97e7ad5970b2e04dba87dfb51ff
mykola-lp <mykola.sukhorab@gmail.com>
2025-08-05 14:11:20 +0300

    project init, tailwind & next configs, base structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import AppHeader from "./components/global/AppHeader";
+import AppFooter from "./components/global/AppFooter";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="max-w-[1280px] mx-auto flex flex-col justify-beween min-h-screen">
+        <AppHeader />
+        
+        <main className="basis-auto grow">
+          {children}
+        </main>
+
+        <AppFooter />
       </body>
     </html>
   );
